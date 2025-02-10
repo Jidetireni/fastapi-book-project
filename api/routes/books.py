3f27688641cfd4e822d13fcc444d04c75daa504c
Jide Tireni <jtirenipraise@gmail.com>
2025-02-10 17:20:46 +0100

    missing endpoint added

--- api/routes/books.py
+++ api/routes/books.py
@@ -67,6 +67,6 @@ async def get_book(book_id: int) -> Book:
     if not book:
         return JSONResponse(
             status_code=status.HTTP_404_NOT_FOUND,
-            content={"message": "Book not found"},
+            content={"detail": "Book not found"},
         )
     return book
